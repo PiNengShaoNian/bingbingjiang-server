519c05612ba1b18e3289943beba91d10e9fc0faf
laoqu <quguiyou1076@qq.com>
2019-10-13 08:36:45 +0800

    delete unnecessary directory

--- bingbingjiang-server/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const DB_URL = 'mongodb://localhost:27017/bingbingjiang'
-
-mongoose.set('useCreateIndex', true)
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
-
-module.exports = mongoose
